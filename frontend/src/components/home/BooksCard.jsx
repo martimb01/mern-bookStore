3ecc3537b592baa1269446ab0f693a1278a7f212
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-10 17:28:57 +0000

    Finished BookCards and BookCard (singular) to clean up codebase

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -5,11 +5,14 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { Link } from 'react-router-dom'
+import BookCard from './BookCard'
 
 const BooksCard = ({books}) => {
     return(
-        <div>
-
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {books.map((item) => (
+                <BookCard key={item._id} book={item} />
+            ))}
         </div>
     )
 }
