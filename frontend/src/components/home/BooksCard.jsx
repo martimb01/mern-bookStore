c287af98fa4b2e78926dac632cc2f8f0ff2ef6e4
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-10 17:16:32 +0000

    Working on the BooksCard component

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import {PiBookOpenTextLight} from 'react-icons/pi'
+import { BiUserCircle } from 'react-icons/bi'
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+import { Link } from 'react-router-dom'
 
-const BooksCard = () => {
+const BooksCard = ({books}) => {
     return(
         <div>
 
